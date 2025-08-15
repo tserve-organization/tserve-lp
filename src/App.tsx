a159b02a356bf16abcf5655a5623d78ede190f0e
jhon-alves <jonatas.a2012@hotmail.com>
2025-08-14 21:47:23 -0300

    tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
+      <Header />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +30,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+      <Footer />
+    </div>
   )
 }
 
